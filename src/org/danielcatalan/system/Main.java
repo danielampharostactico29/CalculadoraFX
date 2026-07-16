0bd703774bb194067c0f24538edede9650b39254
danielcatalan <dcatalan-2026234@kinal.edu.gt>
2026-07-16 11:52:27 -0600

    000 se creo la calculadoraFX

---
+++ src/org/danielcatalan/system/Main.java
@@ -0,0 +1,31 @@
+package org.danielcatalan.system;
+ 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.danielcatalan.view.CalculadoraView;
+ 
+public class Main extends Application {
+ 
+ 
+    public static void main(String[] args) {   
+        System.out.println("Hola kinal esta es mi primera calculadora de java fx"); 
+        launch(args); 
+    }
+ 
+    @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+        //vista de la calculadora 
+        CalculadoraView calculadora = new CalculadoraView(); 
+        //nodos
+        Pane raiz = new Pane(calculadora.getView());
+        //escena(nodoRaiz, ancho, largo)
+        Scene escena = new Scene(raiz, 266, 390); 
+        //cargar escena y mostrar escenario principal
+        escenarioPrincipal.setTitle("Calculadora de Daniel");
+        escenarioPrincipal.setScene(escena); 
+        escenarioPrincipal.show();
+    }
+}
---
+++ src/org/danielcatalan/view/CalculadoraView.java
@@ -0,0 +1,93 @@
+package org.danielcatalan.view;
+ 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+public class CalculadoraView {
+private VBox view; 
+private Label pantalla; 
+private GridPane cuadroBotones; 
+
+//controller 
+    public CalculadoraView() {
+        view = new VBox(15); 
+        view.setPadding(new Insets(15)); 
+        view.setAlignment(Pos.CENTER);
+        view.setStyle("-fx-background-color: #A0D6A7;"); 
+        pantalla = new Label("0"); 
+        pantalla.setFont(Font.font("Consolas", FontWeight.BOLD, 40));
+        pantalla.setAlignment(Pos.CENTER_RIGHT); 
+        pantalla.setPrefSize(235, 50); 
+        cuadroBotones = new GridPane(); 
+        cuadroBotones.setHgap(10);
+        cuadroBotones.setVgap(10);
+        cuadroBotones.setAlignment(Pos.CENTER);
+ 
+        Button btnUno = crearBoton("1");
+        Button btnDos = crearBoton("2");
+        Button btnTres = crearBoton("3");
+        Button btnCuatro = crearBoton("4");
+        Button btnCinco = crearBoton("5");
+        Button btnSeis = crearBoton("6");
+        Button btnSiete = crearBoton("7");
+        Button btnOcho = crearBoton("8");
+        Button btnNueve = crearBoton("9");
+        Button btnCero = crearBoton("0");
+        Button btnSuma = crearBoton("+");
+        Button btnResta = crearBoton("-");
+        Button btnMultiplicacion = crearBoton("x");
+        Button btnDividir = crearBoton("÷");
+        Button btnRaiz = crearBoton("√");
+        Button btnPotencia = crearBoton("x²");
+        Button btnPunto = crearBoton(".");
+        Button btnIgual = crearBoton("=");
+        Button btnClear = crearBoton("C");
+        Button btnBorrar = crearBoton("⌫");
+
+        cuadroBotones.add(btnUno, 0, 4);
+        cuadroBotones.add(btnDos, 1, 4);
+        cuadroBotones.add(btnTres, 2, 4);
+        cuadroBotones.add(btnCuatro, 0, 3);
+        cuadroBotones.add(btnCinco, 1, 3);
+        cuadroBotones.add(btnSeis, 2, 3);
+        cuadroBotones.add(btnSiete, 0, 2);
+        cuadroBotones.add(btnOcho, 1, 2);
+        cuadroBotones.add(btnNueve, 2, 2);
+        cuadroBotones.add(btnCero, 1, 5);
+        cuadroBotones.add(btnSuma, 3, 4);
+        cuadroBotones.add(btnResta, 3, 3);
+        cuadroBotones.add(btnMultiplicacion, 3, 2);
+        cuadroBotones.add(btnPunto, 2, 5);
+        cuadroBotones.add(btnIgual, 3, 5);
+        cuadroBotones.add(btnClear, 0, 5);
+        cuadroBotones.add(btnDividir, 2, 1);
+        cuadroBotones.add(btnRaiz, 0, 1);
+        cuadroBotones.add(btnBorrar, 3, 1);
+        cuadroBotones.add(btnPotencia, 1, 1);
+        
+          
+        view.getChildren().addAll(pantalla, cuadroBotones); 
+    }
+    public VBox getView() {
+        return view; 
+    }
+    public Button crearBoton(String bType){
+    Button btn = new Button(bType);
+    btn.setPrefSize(50, 50);
+    btn.setOnMousePressed(e -> {
+    btn.setStyle("-fx-background-color: " + "#786873"+ "; -fx-text-fill:white; -ft-background-radops:5px");
+    btn.setTranslateY(2);
+    });
+    btn.setOnMouseReleased(e -> {
+    btn.setStyle("-fx-background-color: #9CB8A1; -fx-text-fill:white; -ft-background-radops:5px");
+    btn.setTranslateY(0);
+    });
+    
+    return btn;
+    }    
+    }
